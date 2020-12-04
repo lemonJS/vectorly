540c3b0d31295b685bd3480735a68ff33525c94e
Lewis Monteith <lewismonteith@gmail.com>
2020-12-04 09:13:14 +0000

    Enable a few more tsc defaults

--- src/lib/layout/actions.ts
+++ src/lib/layout/actions.ts
@@ -1,13 +1,14 @@
 import type { Dispatch } from 'redux';
+import type { LayoutAction } from './reducers';
 
 export function setMenuOpen(open: boolean) {
-  return async function(dispatch: Dispatch<any>) {
+  return async function(dispatch: Dispatch<LayoutAction>) {
     dispatch({ type: 'LAYOUT', payload: { menuOpen: open } });
   }
 }
 
 export function setMenuSelected(name: string | null) {
-  return async function(dispatch: Dispatch<any>) {
+  return async function(dispatch: Dispatch<LayoutAction>) {
     dispatch({ type: 'LAYOUT', payload: { menuSelected: name } });
   }
 }
