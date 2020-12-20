90f44fdd35b4ab7516d7b2db2c0b560a7c45305d
Lewis Monteith <lewismonteith@gmail.com>
2020-12-20 09:48:32 +0000

    Fix host

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 module.exports = phase => ({
   publicRuntimeConfig: {
-    apiHostUrl: `https://api.vectorly.app/api/${phase === PHASE_DEVELOPMENT_SERVER ? 'dev' : 'live'}`
+    apiHostUrl: `https://api.vectorly.app/${phase === PHASE_DEVELOPMENT_SERVER ? 'dev' : 'live'}`
   }
 });
