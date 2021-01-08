7b8a86845b390676258ea1553eac41fc37cfd160
Lewis Monteith <lewismonteith@gmail.com>
2021-01-08 14:11:47 +0000

    Fix sticky drawing

--- src/components/svg/drawing/paths.tsx
+++ src/components/svg/drawing/paths.tsx
@@ -35,15 +35,13 @@ export class Paths extends React.Component<Props, State> {
     document.removeEventListener('mousemove', this.handleMouseMove, false);
   }
 
-  private handleMouseUp = (event: MouseEvent) => {
-    const element = event.target as HTMLElement;
-    console.log(element);
-
+  private handleMouseUp = (_event: MouseEvent) => {
     if (this.state.pressed && this.path) {
       const { path, transform } = this.getCreationParams;
       this.props.handleCreate(path, transform);
-      this.setState({ pressed: false, coords: [] });
     }
+
+    this.setState({ pressed: false, coords: [] });
   };
 
   private handleMouseDown = (event: MouseEvent) => {
@@ -58,6 +56,7 @@ export class Paths extends React.Component<Props, State> {
     if (this.state.pressed && this.props.active) {
       const { x, y } = window.canvas.getBoundingClientRect();
       const coords = [event.clientX - x, event.clientY - y];
+
       this.setState(state => ({ coords: [...state.coords, coords] }));
     }
   }, 10);
@@ -80,16 +79,23 @@ export class Paths extends React.Component<Props, State> {
   }
 
   private get getCreationParams() {
+    // Get a list of all the x and y coords
     const xCoords = this.state.coords.map(c => c[0]);
     const yCoords = this.state.coords.map(c => c[1]);
 
+    // Find the smallest of all of them
     const xOffset = Math.min(...xCoords);
     const yOffset = Math.min(...yCoords);
 
+    // Reset them so the path effectively starts at 0, 0
     const coords = this.state.coords.map(c => [c[0] - xOffset, c[1] - yOffset]);
 
+    // Get the path from the coords
     const path = this.convertCoordsToPath(coords);
 
+    // Use the offset to transform the path. This way it will
+    // appear in the exact position, but will not totally shaft
+    // the selection box
     const transform = {
       x: xOffset,
       y: yOffset,
