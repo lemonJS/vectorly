f51ed9bdfaa2e5ad101d5cb73cabfdf4cea3500d
Lewis Monteith <lewismonteith@gmail.com>
2021-07-27 09:46:10 +0100

    Ignore eslint build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  }
+};
