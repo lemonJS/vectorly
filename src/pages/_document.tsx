22f7394870fcdc49f7f4972680bfc912d26d2c21
Lewis Monteith <lewismonteith@gmail.com>
2021-08-23 15:27:07 +0100

    Remove squeaky

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,17 +8,7 @@ class Document extends NextDocument {
   public render(): JSX.Element {
     return (
       <Html>
-        <Head>
-        <script dangerouslySetInnerHTML={{ __html: `
-            (function(s,q,e,a,u,k,y){
-              s._sqSettings={site_id:'c3d8cd48-73cd-482f-96c9-3d78bc31cd60'};
-              u=q.getElementsByTagName('head')[0];
-              k=q.createElement('script');
-              k.src=e+s._sqSettings.site_id;
-              u.appendChild(k);
-            })(window,document,'https://cdn.squeaky.ai/g/0.4.0/script.js?');
-        `}} />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
